d68affb0e98b72e4fc15eb74613df801e4d2e60a
Hiroyuki Hanada <hana-hiro@live.jp>
2017-02-24 20:43:43 +0900

    Added counting nonzero elements and detecting the maximum feature ID

--- libsvmdata_reader.rb
+++ libsvmdata_reader.rb
@@ -31,11 +31,17 @@ if $0 == __FILE__
   ARGV.each do |a|
     begin
       loaded_instances = 0
+      nnz = 0
+      max_feature = 0
       read_libsvm_file(a) do |label, instance_hash|
         loaded_instances += 1
+        nnz += instance_hash.values.select{ |c| c != 0.0 }.size 
+        max_feature = [max_feature, instance_hash.keys.max].max
         puts "#{a}: #{loaded_instances} instances loaded" if loaded_instances % 10000 == 0
       end
       puts "#{a}: #{loaded_instances} instances loaded" if loaded_instances % 10000 != 0
+      puts "Maximum feature ID: #{max_feature == 0 ? 'undefined' : max_feature}"
+      puts "Number of nonzero elements: #{nnz}"
     rescue Exception => e
       puts e
     else
